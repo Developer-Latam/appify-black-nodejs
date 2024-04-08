238d7c6f562028dfbbb627281701c275caaa2a8e
samugauna7job <sgauna@wecom.global>
2024-04-08 16:43:19 -0300

    avance createFV

--- src/persistence/transactions/executeTransaction.js
+++ src/persistence/transactions/executeTransaction.js
@@ -1,12 +1,13 @@
 import { prisma } from "../../utils/dependencys/injection.js";
+import { CustomError } from "../../utils/httpRes/handlerResponse.js";
 async function executeTransactions(operations) {
     try {
+        console.log("ACA ESTAN LAS OPERACIONES QUE LLEGARON A MI FUNCION executeTransactions",operations)
         const result = await prisma.$transaction(operations);
         console.log(`Operaciones completadas exitosamente`);
         return result; // Puede que quieras retornar el resultado directamente sin formatearlo como string
     } catch (error) {
-        console.error("Transaction failed: ", error);
-        throw new Error('Transaction failed');
+        throw new CustomError(500, 'Transaction failed', error);
     }
 }
 
