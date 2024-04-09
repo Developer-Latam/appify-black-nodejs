173cca1cc8169ec880fe9c52beb6d74ab98fef5f
samugauna7job <sgauna@wecom.global>
2024-04-09 10:11:24 -0300

    create FVE terminado

--- src/controllers/administracion/ventasController.js
+++ src/controllers/administracion/ventasController.js
@@ -15,3 +15,17 @@ export const createFVController = async (req, res) => {
         ResponseHandler.HandleError(res,error)
     }
 }
+export const createFVEController = async (req, res) => {
+    try {
+        const data = {
+            documento_venta: req.body.documento_venta,
+            factura_venta_excenta: req.body.factura_venta_excenta,
+            item_servicio_factura_venta_excenta: req.body.item_servicio_factura_venta_excenta,
+            item_producto_factura_venta_excenta: req.body.item_producto_factura_venta_excenta,
+        };
+        const result = await ventasService.createFVE(data);
+        ResponseHandler.Ok(res, result)
+    } catch (error) {
+        ResponseHandler.HandleError(res,error)
+    }
+}
