b7ac3de90e02511b5a8c2b2f517ce52e2a46c5c7
Magellan <magebanyulsgmail.com>
2024-04-15 16:10:19 -0400

    Modificacion prisma item doc venta despacho

--- src/services/administracion/cuentasService.js
+++ src/services/administracion/cuentasService.js
@@ -1,284 +1,134 @@
-import cobrosRepository from "../../persistence/repositorys/administracion/cobrosRepository.js";
+import cuentasRepository from "../../persistence/repositorys/administracion/cuentasRepository.js";
 import { idgenerate } from "../../utils/id/idGenerate.js";
 
-class cobrosService {
+class cuentasService {
 
-    async createCobrosAll(data) {
-        try {
-            let cobroFunction;
-    
-            if (data.cobros_factura_venta) {
-                cobroFunction = this.createCobroFV;
-            } else if (data.cobros_factura_venta_excenta) {
-                cobroFunction = this.createCobroFVE;
-            } else if (data.cobros_factura_nota_credito) {
-                cobroFunction = this.createCobroNC;
-            } else {
-                throw new Error('Tipo de cobro no válido en los datos recibidos');
-            }
-    
-            // Llamar a la función correspondiente para crear el registro de cobro de factura
-            const cobroFV = await cobroFunction(data);
-    
-            // Devolver ambos registros como un arreglo
-            return [cobroFV];
-        } catch (error) {
-            // Manejar cualquier error que pueda ocurrir
-            console.error("Error al crear cobro y cobro de factura de venta/ venta excenta / nota de credito:", error);
-            throw error; // Relanzar el error para que pueda ser manejado por el código que llama a esta función
-        }
-    }
-    
-
-    async createCobroFV(data) {
-        try {
-            // Generar IDs para el cobro y el cobro de factura de venta
-            const cobroId = idgenerate("Cobro");
-            const cobroFVId = idgenerate("Cobro-FV");
-
-            // Extraer datos del objeto data
-            const { cobros, cobros_factura_venta } = data;
-    
-            // Crear el registro de cobro
-            const cobro = await cobrosRepository.createCobros({ ...cobros, id: cobroId });
-    
-            // Agregar el ID del cobro al objeto de datos para el cobro de factura de venta
-            const cobroFVData = { ...cobros_factura_venta, id: cobroFVId, idCobro: cobroId };
-    
-            // Crear el registro de cobro de factura de venta
-            const cobroFV = await cobrosRepository.createCobrosFV(cobroFVData);
-    
-            // Devolver ambos registros como un arreglo
-            return [cobro, cobroFV];
-        } catch (error) {
-            // Manejar cualquier error que pueda ocurrir
-            console.error("Error al crear cobro y cobro de factura de venta:", error);
-            throw error; // Relanzar el error para que pueda ser manejado por el código que llama a esta función
-        }
+    async createCuentasBanco(data) {
+        const id = idgenerate("Cuenta-banco");
+        
+        return  cuentasRepository.createCuentaBanco({ ...data, id: id });
     }
 
-    async createCobroFVE(data) {
-        try {
-            // Generar IDs para el cobro y el cobro de factura de venta
-            const cobroId = idgenerate("Cobro");
-            const cobroFVEId = idgenerate("Cobro-FVE");
-
-            // Extraer datos del objeto data
-            const { cobros, cobros_factura_venta_excenta } = data;
-    
-            // Crear el registro de cobro
-            const cobro = await cobrosRepository.createCobros({ ...cobros, id: cobroId });
-    
-            // Agregar el ID del cobro al objeto de datos para el cobro de factura de venta
-            const cobroFVEData = { ...cobros_factura_venta_excenta, id: cobroFVEId, idCobro: cobroId };
-    
-            // Crear el registro de cobro de factura de venta
-            const cobroFVE = await cobrosRepository.createCobrosFVE(cobroFVEData);
-    
-            // Devolver ambos registros como un arreglo
-            return [cobro, cobroFVE];
-        } catch (error) {
-            // Manejar cualquier error que pueda ocurrir
-            console.error("Error al crear cobro y cobro de factura de venta:", error);
-            throw error; // Relanzar el error para que pueda ser manejado por el código que llama a esta función
-        }
+    async createCuentaBancoConciliacion(data) {
+        const id = idgenerate("Cuenta-banco-conciliacion");
+        
+        return  cuentasRepository.createCuentaBancoConciliacion({ ...data, id: id });
     }
 
-    async createCobroNC(data) {
-        try {
-            // Generar IDs para el cobro y el cobro de factura de venta
-            const cobroId = idgenerate("Cobro");
-            const cobroNCId = idgenerate("Cobro-NC");
-
-            // Extraer datos del objeto data
-            const { cobros, cobros_factura_nota_credito } = data;
-    
-            // Crear el registro de cobro
-            const cobro = await cobrosRepository.createCobros({ ...cobros, id: cobroId });
-    
-            // Agregar el ID del cobro al objeto de datos para el cobro de factura de venta
-            const cobroNCData = { ...cobros_factura_nota_credito, id: cobroNCId, idCobro: cobroId };
-    
-            // Crear el registro de cobro de factura de venta
-            const cobroNC = await cobrosRepository.createCobrosNC(cobroNCData);
-    
-            // Devolver ambos registros como un arreglo
-            return [cobro, cobroNC];
-        } catch (error) {
-            // Manejar cualquier error que pueda ocurrir
-            console.error("Error al crear cobro y cobro de factura de venta:", error);
-            throw error; // Relanzar el error para que pueda ser manejado por el código que llama a esta función
-        }
+    async createCategoriaCuenta(data) {
+        const id = idgenerate("Categoria-cuenta");
+        
+        return  cuentasRepository.createCategoriaCuenta({ ...data, id: id });
     }
 
-    async getCobrosAllById(id) {
-        const functionsToTry = [
-            this.getCobroFVById,
-            this.getCobroFVEById,
-            this.getCobroNCById
-        ];
-    
-        for (const func of functionsToTry) {
-            try {
-                const result = await func(id);
-                // Si la función no arroja error y devuelve algo, retornamos el resultado
-                return result;
-            } catch (error) {
-                // Si hay un error, continuamos con la siguiente función
-                console.error(`Error al intentar ejecutar la función ${func.name}:`, error.message);
-            }
-        }
-    
-        // Si ninguna función devuelve nada sin error, lanzamos una excepción
-        throw new Error('No se encontró ningún cobro para el ID proporcionado');
+    async createCuentaTipoDocumento(data) {
+        const id = idgenerate("Cuenta-tipo-doc");
+        
+        return  cuentasRepository.createCuentaTipoDocumento({ ...data, id: id });
     }
 
-    async getCobroFVById(id) {
-        try {
-            // Buscar el cobro
-            const cobro = await cobrosRepository.findCobroById(id);
-    
-            if (!cobro) {
-                throw new Error(`No se encontró ningún cobro con el ID ${id}`);
-            }
-    
-            // Buscar el cobro de factura de venta utilizando el ID del cobro
-            const cobroFV = await cobrosRepository.findCobroFVByCobroId(id);
-    
-            if (!cobroFV) {
-                throw new Error(`No se encontró ningún cobro de factura de venta para el cobro con ID ${id}`);
-            }
-    
-            // Devolver un objeto que contiene ambos resultados
-            return { cobro, cobroFV };
+    async createBanco(data) {
+        const id = idgenerate("Banco-");
+        
+        return  cuentasRepository.createBanco({ ...data, id: id });
+    }
+
+    async createCondicionPago(data) {
+
+        try{
+            const id = idgenerate("Condicion-pago");
+            const idCPago = idgenerate("Condiciones-Pago")
+
+            const {condicion_pago, condiciones_condicion_pago} = data;
+
+            const CondicionPago = await cuentasRepository.createCondicionPago({ ...condicion_pago, id: id });
+
+            const CondicionesCondicionPago = await cuentasRepository.createCondicionesCondicionPago({ ...condiciones_condicion_pago, id: idCPago });
+
+            return [CondicionPago, CondicionesCondicionPago];
         } catch (error) {
-            console.error("Error al buscar el cobro y cobro de factura de venta:", error.message);
+            console.error("Error al crear condicion de pago y respectivas condiciones:", error);
             throw error;
         }
+        
     }
-
-    async getCobroFVEById(id) {
-        try {
-            // Buscar el cobro
-            const cobro = await cobrosRepository.findCobroById(id);
     
-            if (!cobro) {
-                throw new Error(`No se encontró ningún cobro con el ID ${id}`);
-            }
-    
-            // Buscar el cobro de factura de venta utilizando el ID del cobro
-            const cobroFVE = await cobrosRepository.findCobroFVEByCobroId(id);
-    
-            if (!cobroFVE) {
-                throw new Error(`No se encontró ningún cobro de factura de venta excenta para el cobro con ID ${id}`);
-            }
-    
-            // Devolver un objeto que contiene ambos resultados
-            return { cobro, cobroFVE };
-        } catch (error) {
-            console.error("Error al buscar el cobro y cobro de factura de venta excenta:", error.message);
-            throw error;
-        }
+
+    async getCuentaBancoById(id) {
+        return cuentasRepository.findCuentaBancoById(id);
     }
 
-    async getCobroNCById(id) {
-        try {
-            // Buscar el cobro
-            const cobro = await cobrosRepository.findCobroById(id);
-    
-            if (!cobro) {
-                throw new Error(`No se encontró ningún cobro con el ID ${id}`);
-            }
-    
-            // Buscar el cobro de factura de venta utilizando el ID del cobro
-            const cobroNC = await cobrosRepository.findCobroNCByCobroId(id);
-    
-            if (!cobroNC) {
-                throw new Error(`No se encontró ningún cobro de nota de credito para el cobro con ID ${id}`);
-            }
-    
-            // Devolver un objeto que contiene ambos resultados
-            return { cobro, cobroNC };
-        } catch (error) {
-            console.error("Error al buscar el cobro y cobro de nota de credito:", error.message);
-            throw error;
-        }
+    async getCuentaBancoConciliacionId(id) {
+        return cuentasRepository.findCuentaBancoConciliacionById(id);
     }
 
-    async getCobrosByUserId(userId) {
-        return cobrosRepository.findAllCobrosByUserId(userId);
+    async getCobrosByUserId(id) {
+        return cuentasRepository.findCuentaBancoById(id);
     }
 
-    async updateCobro(id, updateData) {
-        return cobrosRepository.updateCobro(id, updateData);
+    async getCategoriaCuentaByIdId(id) {
+        return cuentasRepository.findCategoriaCuentaById(id);
     }
 
-    async updateCobroFV(id, updateData) {
-        return cobrosRepository.updateCobroFV(id, updateData);
+    async getCuentaTipodocumentoById(id) {
+        return cuentasRepository.findCuentaTipodocumentoById(id);
     }
 
-    async updateCobroFVE(id, updateData) {
-        return cobrosRepository.updateCobroFVE(id, updateData);
+    async getCondicionPagoById(id) {
+        return cuentasRepository.findCondicionPagoById(id);
     }
 
-    async updateCobroNC(id, updateData) {
-        return cobrosRepository.updateCobroNC(id, updateData);
+    async getCondicionesCondicionPagoById(id) {
+        return cuentasRepository.findCondicionesCondicionPagoById(id);
     }
 
-
-
-    async deleteCobroFV(id) {
-        try {
-            const cobro = await cobrosRepository.findCobroFVByCobroId(id);
-            if (!cobro) {
-                throw new Error(`No se encontró ningún cobro de factura de venta con el ID ${id}`);
-            }
-            console.log(cobro.id);
-            const delCobroFV = await cobrosRepository.deleteCobroFVByCobroId(cobro.id);
-            const delCobro = await cobrosRepository.deleteCobro(id);
-            
-            return [delCobro, delCobroFV];
-        } catch (error) {
-            console.error("Error al eliminar el cobro y cobro de factura de venta:", error.message);
-            throw error;
-        }
+    async getAllCuentasBancoByUserId(id) {
+        return cuentasRepository.findAllCuentasBancoByUserId(id);
     }
 
-    async deleteCobroFVE(id) {
-        try {
-            const cobro = await cobrosRepository.findCobroFVEByCobroId(id);
-            if (!cobro) {
-                throw new Error(`No se encontró ningún cobro de factura de venta con el ID ${id}`);
-            }
-            console.log(cobro.id);
-            const delCobroFV = await cobrosRepository.deleteCobroFVEByCobroId(cobro.id);
-            const delCobro = await cobrosRepository.deleteCobro(id);
-            
-            return [delCobro, delCobroFV];
-        } catch (error) {
-            console.error("Error al eliminar el cobro y cobro de factura de venta:", error.message);
-            throw error;
-        }
+    async getAllCuentasBancoConciliacionByUserId(id) {
+        return cuentasRepository.findAllCuentasBancoConciliacionByUserId(id);
     }
 
-
-    async deleteCobroNC(id) {
-        try {
-            const cobro = await cobrosRepository.findCobroNCByCobroId(id);
-            if (!cobro) {
-                throw new Error(`No se encontró ningún cobro de factura de venta con el ID ${id}`);
-            }
-            console.log(cobro.id);
-            const delCobroFV = await cobrosRepository.deleteCobroNCByCobroId(cobro.id);
-            const delCobro = await cobrosRepository.deleteCobro(id);
-            
-            return [delCobro, delCobroFV];
-        } catch (error) {
-            console.error("Error al eliminar el cobro y cobro de factura de venta:", error.message);
-            throw error;
-        }
+    async getAllCategoriasCuentaByUserId(id) {
+        return cuentasRepository.findAllCategoriasCuentaByUserId(id);
     }
 
+    async getAllCuentasTipoDocumentoByUserId(id) {
+        return cuentasRepository.findAllCuentaTipoDocumentoByUserId(id);
+    }
+
+    async getAllBancosByUserId(id) {
+        return cuentasRepository.findAllBancosByUserId(id);
+    }
+
+    async getAllCondicionPagoByUserId(id) {
+        return cuentasRepository.findAllCondicionPagoByUserId(id);
+    }
+
+    async updateCuentasBanco(id, updateData) {
+        return cuentasRepository.updateCuentasBanco(id, updateData);
+    }
+
+    async updateCategoriaCuenta(id, updateData) {
+        return cuentasRepository.updateCategoriaCuenta(id, updateData);
+    }
+
+    async updateCuentaTipoDoc(id, updateData) {
+        return cuentasRepository.updateCuentaTipoDoc(id, updateData);
+    }
+
+    async updateBanco(id, updateData) {
+        return cuentasRepository.updateBanco(id, updateData);
+    }
+
+    async updateCondicionPago(id, updateData) {
+        return cuentasRepository.updateCondicionPago(id, updateData);
+    }
+
+    async updateCondicionesCondicionPago(id, updateData) {
+        return cuentasRepository.updateCondicionesCondicionPago(id, updateData);
+    }
 
 
 
@@ -286,4 +136,4 @@ class cobrosService {
 }
 
 
-export default new cobrosService()
+export default new cuentasService()
