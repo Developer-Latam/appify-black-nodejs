cccc63cdf277c0883c55c12fa0c2e83753d7764e
Magellan <magebanyulsgmail.com>
2024-05-03 09:04:15 -0400

    modificado el CORS

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ import 'dotenv/config'
 const app = express()
 const PORT = process.env.PORT || 8080;
 
-app.use(cors({origin: 'http://localhost:5173'}))
+app.use(cors())
 app.use(express.urlencoded({extended:true}))
 
 app.use(express.json())
